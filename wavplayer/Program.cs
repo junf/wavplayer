25de1a7ff3630399aab1439207439eb3165331f9
Jun Funada <jun2@alt-area.flop.jp>
2013-05-06 15:23:19 +0900

    first commit

---
+++ wavplayer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wavplayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = args.Length;
+            if (n < 1)
+            {
+                Console.WriteLine("Usage: wavplayer wavfile");
+            }
+            else
+            {
+                SoundPlayer wavePlayer = new SoundPlayer(args[0]);
+                wavePlayer.PlaySync();
+            }
+        }
+    }
+}
